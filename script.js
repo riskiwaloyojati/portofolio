695a7ce9c20712a11d7b6b97a475709f8162fab0
riskiwaloyojati <riskiwaloyojati@gmail.com>
2024-07-24 22:48:04 +0700

    fetch('portofolio'): tambah template image slide
    Masih berantakan. Butuh perbaikan pada CSS dan menyiapkan konten portofolio.

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
--- test/test.js
+++ test/test.js
@@ -1,21 +1,17 @@
-const swiper = new Swiper('.swiper', {
-// Optional parameters
-direction: 'vertical',
-loop: true,
-
-// If we need pagination
-pagination: {
-el: '.swiper-pagination',
-},
-
-// Navigation arrows
-navigation: {
-nextEl: '.swiper-button-next',
-prevEl: '.swiper-button-prev',
-},
-
-// And if we need scrollbar
-scrollbar: {
-el: '.swiper-scrollbar',
-},
-});
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 350,
+      modifier: 1,
+      slideShadows: true
+    },
+    pagination: {
+      el: ".swiper-pagination"
+    }
+  });
+  
